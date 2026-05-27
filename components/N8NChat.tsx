554f0d796b57aed4db67903bcecbb1de57815c2f
shubsemwal <shubsem34@gmail.com>
2026-05-27 13:56:33 +0530

    first commit

--- components/N8NChat.tsx
+++ components/N8NChat.tsx
@@ -8,7 +8,7 @@ export default function N8NChat() {
     import("@n8n/chat").then(({ createChat }) => {
       createChat({
         webhookUrl:
-          "https://sem5115.app.n8n.cloud/webhook/d97ceda9-5bf5-4036-95c0-3c11e1b4fc9e/chat",
+          "https://sem5115.app.n8n.cloud/webhook/07295e22-9127-4ea7-a06c-7eaba50505f1/chat",
       });
     });
   }, []);
